ee1a1e0652505c2bb54c5a0a979de2272d25a110
Donghyun Lee <yahopet79@gmail.com>
2025-01-03 01:04:36 +0900

    first commit

---
+++ src/main/java/com/examples/mytv/MyTvApplication.java
@@ -0,0 +1,13 @@
+package java.com.examples.mytv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examples/mytv/VideoWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.examples.mytv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
